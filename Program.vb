18747844591aad079951c5ac6c70dfdc342680b8
Ward Childress <ward.childress@gmail.com>
2020-03-02 17:15:53 -0500

    The rest of the Hello World! tutorial.

--- Program.vb
+++ Program.vb
@@ -3,5 +3,7 @@ Imports System
 Module Program
     Sub Main(args As String())
         Console.WriteLine("Hello World!")
+        Console.WriteLine("Press any key to continue...")
+        Console.ReadKey(True)
     End Sub
 End Module
