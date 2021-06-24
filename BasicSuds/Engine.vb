72a7159155a83ea01e8ae77e4124a094924a9833
Ward Childress <ward.childress@gmail.com>
2021-06-24 15:22:14 -0400

    Big update including automatic text wrapping, better look handling, more rooms in the demo game, and much more cleanup.
    Added support for automatic text wrapping of descriptions based on the current window width. Implemented a look command, expanded the help, colored error messages in red, and other cleanup around the codebase. Many more rooms have been created for the demo game, now called Northern Lights.

--- BasicSuds/Engine.vb
+++ BasicSuds/Engine.vb
@@ -4,11 +4,14 @@ Namespace BasicSuds
     Public Class Engine
         Private GameLoaded As Boolean = False
         Private Running As Boolean = False
+        Private NeedLook As Boolean = False
+        Private Version As String = "alpha.210624"
 
         Private Rooms As New Dictionary(Of String, Room)
         Private CurrentRoom As String
 
         Public Sub LoadGame(gameFilename As String)
+            Console.WriteLine($"Welcome to the BasicSuds retro engine for Single User Dungeons, version {Version}!")
             Console.WriteLine($"Loading Single User Dungeon ""{gameFilename}""...")
             Try
                 Dim gameXml As XElement = XElement.Load(gameFilename)
@@ -18,26 +21,38 @@ Namespace BasicSuds
                 CurrentRoom = gameXml.@startingRoom
 
                 GameLoaded = True
-                Console.WriteLine($"Ready to play ""{gameXml.@name}""!")
+                Console.Title = $"BasicSuds {Version} - {gameXml.@name}"
+                Console.WriteLine($"{Rooms.Count} rooms loaded. Ready to play ""{gameXml.@name}""!")
             Catch exception As System.IO.FileNotFoundException
+                Console.ForegroundColor = ConsoleColor.Red
                 Console.WriteLine("Game not found!")
+                Console.ResetColor()
             Catch exception As System.Xml.XmlException
+                Console.ForegroundColor = ConsoleColor.Red
                 Console.WriteLine("Malformed XML file!")
+                Console.ResetColor()
             End Try
         End Sub
 
         Public Sub Run()
             If Not GameLoaded Then
+                Console.ForegroundColor = ConsoleColor.Red
                 Console.WriteLine("No game has been loaded!")
+                Console.ResetColor()
                 Return
             End If
+
             Running = True
+            NeedLook = True
 
             ' Main game loop.
             Do While Running
                 Console.WriteLine()
-                Console.WriteLine(Rooms.Item(CurrentRoom).Name)
-                Console.WriteLine(Rooms.Item(CurrentRoom).Description)
+                If NeedLook Then
+                    Console.WriteLine(Rooms.Item(CurrentRoom).Name)
+                    Rooms.Item(CurrentRoom).WriteDescription()
+                    NeedLook = False
+                End If
                 Console.Write("> ")
                 Dim input = Console.ReadLine()
                 HandleInput(input)
@@ -48,13 +63,13 @@ Namespace BasicSuds
         Private Function LookupShorthand(input As String) As String
             Dim shorthand = New Dictionary(Of String, String) From {
                 {"n", "north"},
-                {"s", "south"},
-                {"e", "east"},
-                {"w", "west"},
                 {"ne", "northeast"},
-                {"nw", "northwest"},
+                {"e", "east"},
                 {"se", "southeast"},
+                {"s", "south"},
                 {"sw", "southwest"},
+                {"w", "west"},
+                {"nw", "northwest"},
                 {"u", "up"},
                 {"d", "down"}
                 }
@@ -79,28 +94,19 @@ Namespace BasicSuds
 
             Select Case input
                 Case "exit", "quit"
+                    Console.WriteLine("Good-bye!")
                     Running = False
                     inputUnderstood = True
                 Case "help", "?"
-                    Console.WriteLine("BasicSuds Basic Help")
-                    Console.WriteLine("Type ""exit"" or ""quit"" to finish playing.")
-                    Console.WriteLine("     ""help"" or ""?"" to see these instructions.")
+                    inputUnderstood = DoHelp()
+                Case "look"
+                    NeedLook = True
                     inputUnderstood = True
                 Case ""
                     inputUnderstood = True
                 Case Else
                     ' Room exits can be non-standard commands. Handle checking those here.
-                    If Rooms.Item(CurrentRoom).Exits.ContainsKey(input) Then
-                        Dim newRoom As String = Rooms.Item(CurrentRoom).Exits.Item(input)
-                        If Rooms.ContainsKey(newRoom) Then
-                            CurrentRoom = newRoom
-                        Else
-                            Console.WriteLine($"Room ""{newRoom}"" wasn't found!")
-                        End If
-                    Else
-                        Console.WriteLine($"You can't ""{input}"" here.")
-                    End If
-                    inputUnderstood = True
+                    inputUnderstood = TryExit(input)
             End Select
 
             ' TODO: This is no longer necessary, I think...
@@ -108,5 +114,31 @@ Namespace BasicSuds
                 Console.WriteLine("Huh?")
             End If
         End Sub
+
+        Private Function DoHelp() As Boolean
+            Console.WriteLine("Type ""exit"" or ""quit"" to finish playing.")
+            Console.WriteLine("     ""help"" or ""?"" to see these instructions.")
+            Console.WriteLine("     ""look"" to look around at your surroundings.")
+            Console.WriteLine("     ""north"", ""n"", ""south"", etc to move around the environment.")
+            Return True
+        End Function
+
+        Private Function TryExit(input As String) As Boolean
+            If Rooms.Item(CurrentRoom).Exits.ContainsKey(input) Then
+                Dim newRoom As String = Rooms.Item(CurrentRoom).Exits.Item(input)
+                If Rooms.ContainsKey(newRoom) Then
+                    CurrentRoom = newRoom
+                    NeedLook = True
+                Else
+                    Console.ForegroundColor = ConsoleColor.Red
+                    Console.WriteLine($"Room ""{newRoom}"" wasn't found!")
+                    Console.ResetColor()
+                End If
+            Else
+                Console.WriteLine($"You can't ""{input}"" here.")
+            End If
+
+            Return True
+        End Function
     End Class
 End Namespace
--- Program.vb
+++ Program.vb
@@ -1,8 +1,7 @@
 Friend Module Program
     Public Sub Main()
-        Console.WriteLine("Welcome to the BasicSuds retro engine for Single User Dungeons!")
         Dim engine As New BasicSuds.Engine
-        engine.LoadGame("DemoGame.xml")
+        engine.LoadGame("NorthernLights.xml")
         engine.Run()
     End Sub
 End Module
